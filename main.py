7b39f491e64271e164959310169a4e9e22acbe55
healthyCoder <36478181+healthyCoder@users.noreply.github.com>
2018-08-30 19:34:59 +0530

    Better visuals for main window
    -Major improvements for aestectics and looks in the main window.
    
    -Removed exception handling for better debugging.

--- main.py
+++ main.py
@@ -8,22 +8,23 @@ import tkinter
 from tkinter.filedialog import askopenfilename
 from tkinter import ttk, messagebox
 
-global Xpend
+WINDOW_SIZE="500x300"
+
+global Xpense
 
 class hello:
     def __init__(self):
         self.buttonHeight=20
         self.buttonWidth=50
-        self.file1 = open('ords.txt', "a")
-        self.root = tkinter.Tk("Xpend")
-        self.root.geometry("500x300")
-        #this makes the main window
-        #self.root.title("")
-        self.root.iconbitmap(self, default="images\icon.ico")
-        self.topFrame=Frame(self.root)
-        self.topFrame.pack()
-        self.bottomFrame=Frame(self.root)
-        self.bottomFrame.pack(side="bottom")
+        self.file1 = open('data.txt', "a")
+        self.root = tkinter.Tk()
+        self.root.title("Xpend")
+        self.root.geometry(WINDOW_SIZE)
+        self.root.resizable(0,0)
+        self.root.iconbitmap(self, default="images\icon2.ico")
+        self.mainFrame=Frame(self.root,bg="#15c674")
+        self.mainFrame.pack_propagate(0)
+        self.mainFrame.pack(fill="both",expand=1)
         self.mainp()
         self.root.mainloop()
 
@@ -34,32 +35,31 @@ class hello:
         text1 = str((mamt, mtag, time))
         self.file1.write(text1)
         self.label2 = Label(self.manualw, text="Transaction is successfully saved.")
-        self.label2.grid(row=6, columnspan=3)
+        self.label2.grid(row=6, columnspan=3,padx=3,pady=3)
         self.file1.close()
         self.manualw.after(2000, lambda: self.manualw.destroy())
 
     def manualp(self):
         self.manualw = Tk()
         self.manualw.geometry("500x300")
-        Label(self.manualw, text="Enter the amount").grid(row=0)
+        Label(self.manualw, text="Enter the amount").grid(row=0,padx=3,pady=3)
         self.entryamt = Entry(self.manualw)
-        self.entryamt.grid(row=0, column=1)
-        Label(self.manualw, text="Enter the tag").grid(row=1)
+        self.entryamt.grid(row=0, column=1,padx=3,pady=3)
+        Label(self.manualw, text="Enter the tag").grid(row=1,padx=3,pady=3)
         self.entrytag = Entry(self.manualw)
-        self.entrytag.grid(row=1, column=1)
+        self.entrytag.grid(row=1, column=1,padx=3,pady=3)
         self.bsubmit = ttk.Button(self.manualw, text="Enter into file", command=self.but1fun)
-        self.bsubmit.grid(row=4, columnspan=3)
+        self.bsubmit.grid(row=4, columnspan=3,padx=3,pady=3)
         # self.bclear=Button(self.manualw,text="Clear contents",command=self.file1.truncate(0))
         # self.bclear.grid(row=5,columnspan=3)
         self.manualw.mainloop()
 
     def clickp(self):
-        try:
-            file_path = str(askopenfilename(initialdir="images\Sample Bills", title="Select bill image",
+
+        file_path = str(askopenfilename(initialdir="images\Sample Bills", title="Select bill image",
                                             filetypes=[("Image Files", "*.jpeg;*.jpg;*.png")]))
-            final_image(str(file_path))
-        except:
-            messagebox.showerror("Error","Image was not selected")
+        final_image(str(file_path))
+        #messagebox.showerror("Error","Image was not selected")
 
         """
         amt=final_image(str(file_path))
@@ -78,10 +78,12 @@ class hello:
 
     def mainp(self):
         #this is the first window which displays as soon as the program runs
-        self.manualb = ttk.Button(self.root, text="Enter transaction manually", command=self.manualp)
-        self.manualb.pack()
-        self.clickb = ttk.Button(self.root, text="Enter transaction by picture", command=self.clickp)
-        self.clickb.pack()
+        self.photo = tkinter.PhotoImage(file="images\logo.gif")
+        Label(self.mainFrame,image=self.photo).pack(pady=(0,15))
+        self.manualb = ttk.Button(self.mainFrame, text="Enter transaction manually", command=self.manualp)
+        self.manualb.pack(padx=3,pady=3)
+        self.clickb = ttk.Button(self.mainFrame, text="Enter transaction by picture", command=self.clickp)
+        self.clickb.pack(padx=3,pady=3)
         #the following comments are commented out as it belongs to message reading which will be added on in a later build
 
         #self.msgb = Button(self.root, text="Enter transaction from messages", command=self.msgp)
