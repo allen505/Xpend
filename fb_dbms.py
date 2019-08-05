ca07f70215bd11c9521fc9827cc44bbaff018bb4
AB047 <atulmb99@gmail.com>
2019-08-05 22:16:54 +0530

    Changed codebase from Python to Flutter
    Codebase has been rewritten to boilerplate flutter app and will be continued to be worked on flutter.

--- fb_dbms.py
+++
@@ -1,5 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("xpend-5acc1-firebase-adminsdk-qb7eh-92a96ef78c.json")
-firebase_admin.initialize_app(cred)
